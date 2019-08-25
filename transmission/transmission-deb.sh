c3c6f2d38a0ca63dc358444c9ff32bc19ed45b2c
tnnd <37441897+tnnd@users.noreply.github.com>
2019-08-25 23:21:03 +0800

    SETTINGS.JSON

--- transmission/transmission-deb.sh
+++ transmission/transmission-deb.sh
@@ -2,9 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-apt-get update && apt-get install -y transmission-daemon
-cd /var/lib/transmission-daemon/info/ && wget -N https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json
-service transmission-daemon reload
+apt-get update && apt-get install -y transmission-daemon \
+        && cd /var/lib/transmission-daemon/info/ \
+        && wget -N https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json \
+        && service transmission-daemon reload
+
 # SETTINGS.JSON
 # sed -i 's/^.*rpc-authentication-required.*/"rpc-authentication-required": 'false',/' /var/lib/transmission-daemon/info/settings.json
 # sed -i 's/^.*rpc-whitelist-enabled.*/"rpc-whitelist-enabled": 'false',/' /var/lib/transmission-daemon/info/settings.json
