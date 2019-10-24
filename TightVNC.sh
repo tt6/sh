547151ff9a130c77c8106a81837699e9a262a21f
tnnd <37441897+tnnd@users.noreply.github.com>
2019-10-24 14:34:27 +0800

    Update TightVNC.sh
    lxde-core

--- TightVNC.sh
+++ TightVNC.sh
@@ -3,9 +3,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 # install x-windows, lxde-core / openbox / xfvb(xterm)
-apt-get update && apt-get install -y openbox tightvncserver fonts-wqy-microhei screen
+apt-get update && apt-get install -y dialog lxde-core tightvncserver fonts-wqy-microhei screen
 
-# cd ~/ && git clone https://github.com/tnnd/noVNC.git
+# git clone https://github.com/tnnd/noVNC.git
 
 ## /etc/rc.local
 # /bin/su -l cabox -c "perl /usr/bin/vncserver &" &
