1b4f26101992ff43ff50b3f1cc4e1dc82eef1a98
tnnd <37441897+tnnd@users.noreply.github.com>
2020-05-15 14:35:45 +0800

    Update TightVNC.sh

--- TightVNC.sh
+++ TightVNC.sh
@@ -12,6 +12,8 @@ apt-get update && apt-get install -y dialog lxde-core tightvncserver fonts-wqy-m
 # bash /home/cabox/noVNC/utils/launch.sh --vnc localhost:5901 &
 
 # install nwjs
+# wget -O - https://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-linux-x64.tar.gz | tar zxf -
 apt-get install -y libnss3 libgconf-2-4 libnotify4 libnss3 libXss1 libgtk2.0-0:i386
 echo "wget -O - https://dl.nwjs.io/v0.15.4/nwjs-v0.15.4-linux-x64.tar.gz | tar zxf -"
 echo "nw --url=https://google.com/?hl=zh_cn"
+
