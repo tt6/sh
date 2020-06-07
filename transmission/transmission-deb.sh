5f53d2ab53bc505014497444337cca9d803c6367
tnnd <37441897+tnnd@users.noreply.github.com>
2020-06-07 17:30:08 +0800

    Update transmission-deb.sh

--- transmission/transmission-deb.sh
+++ transmission/transmission-deb.sh
@@ -3,9 +3,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 apt-get update
-apt-get install -y transmission-daemon ca-certificates \
+apt-get install -y transmission-daemon \
         && cd /var/lib/transmission-daemon/info/ \
-        && curl -Ok https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json \
+        && curl -LJOk https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json \
         && service transmission-daemon reload
 
 # SETTINGS.JSON
