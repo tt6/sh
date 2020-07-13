89f66c7b772ca02433a4d6f76763021fd4a1bffc
tnnd <37441897+tnnd@users.noreply.github.com>
2020-07-13 16:20:06 +0800

    Create gcloud.sh

---
+++ transmission/gcloud.sh
@@ -0,0 +1,3 @@
+gcloud compute firewall-rules create http --allow tcp:9091-11413
+gcloud compute instances create ttg --machine-type n1-standard-2 --zone asia-east2-a --boot-disk-size 200 --boot-disk-type pd-ssd
+gcloud compute ssh ttg --zone asia-east2-a --command 'curl -L https://git.io/fhjPW |sudo bash &'
