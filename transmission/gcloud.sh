bfdbe651868562f24a823c563136ac06763a70ec
tnnd <37441897+tnnd@users.noreply.github.com>
2020-07-23 17:42:56 +0800

    Update gcloud.sh

--- transmission/gcloud.sh
+++ transmission/gcloud.sh
@@ -1,6 +1,8 @@
 gcloud compute firewall-rules create http --allow tcp:9091-11413
-gcloud compute instances create ttg --machine-type n1-standard-2 --zone asia-east2-a --boot-disk-size 200 --boot-disk-type pd-ssd
 gcloud compute ssh ttg --zone asia-east2-a --command 'curl -L https://git.io/fhjPW |sudo bash &'
+gcloud compute ssh ttg2 --zone asia-east2-a --command 'curl -L https://git.io/fhjPW |sudo bash &'
+read -n1 -p "Press any key to continue..."
+gcloud compute ssh ttg --zone asia-east2-a --command 'sudo apt install -y xfce4 xrdp qbittorrent && sudo apt purge -y xserver-xorg-legacy &'
+gcloud compute ssh ttg --zone asia-east2-a --command 'sudo useradd -m -p "33rvTQ31Fvuj6" AAA &'
+gcloud compute instances list
 gcloud compute ssh ttg --zone asia-east2-a
-#Debian xfce4
-echo "sudo -i && apt install -y xfce4 xrdp qbittorrent && apt purge xserver-xorg-legacy && useradd -m -p "33rvTQ31Fvuj6" ttg"
